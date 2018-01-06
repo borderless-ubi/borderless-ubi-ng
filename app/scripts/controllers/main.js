b5490a1a942b154d0bd74e61726a9f93a9d86bf5
Rob Thijssen <rthijssen@gmail.com>
2018-01-06 18:38:20 +0200

    yeoman scaffold

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name borderlessUbiNgApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the borderlessUbiNgApp
+ */
+angular.module('borderlessUbiNgApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
